1e643c8d2360ec136b53b52cb93381d434a7b0c9
Elberd Chakhkiev <114188050+eckeriaue@users.noreply.github.com>
2022-11-19 18:32:58 +0300

    bundle update

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import Space from './Space'
 import Panel from './Panel'
 
 import config from './config'
-const canvas: HTMLElement = document.getElementById('c')
+
 const world = new Space({
-  canvas: canvas,
+  canvas: document.getElementById('c') as HTMLElement,
   name: 'wrld',
   params: config
 })
 
 new Panel(config)
 
-window.addEventListener("resize", () => world.resize());
-window.addEventListener("load", () => world.resize());
-world.resize();
+window.addEventListener("resize", () => world.resize())
+window.addEventListener("load", () => world.resize())
+world.resize()
